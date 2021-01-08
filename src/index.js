033b93312b3dfa0b0fada6c915be4ed04f62fce5
Mert Dogar <mertdogar@gmail.com>
2021-01-08 11:59:06 +0300

    :sparkles: Separate initial store creation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import isEqual from 'lodash/isEqual';
+import _ from 'lodash';
+import get from 'lodash/get';
 import isUndefined from 'lodash/isUndefined';
+import isFunction from 'lodash/isFunction';
 import assign from 'lodash/assign';
 import {useState, useEffect} from 'react';
 import EventEmitterExtra from 'event-emitter-extra';
+import {isEquivalent} from './utils';
+
+_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+
+EventEmitterExtra.defaultMaxListeners = Infinity;
 
 class Stores extends EventEmitterExtra {
   storeBank = {};
@@ -11,31 +18,100 @@ class Stores extends EventEmitterExtra {
     return this.storeBank[name];
   }
 
-  update(name, nextState) {
-    if (!this.get(name)) this.storeBank[name] = {};
-    if (isEqual(this.get(name), nextState)) return;
-    assign(this.storeBank[name], nextState);
-    this.emit(`update_${name}`, this.get(name));
+  set(name, nextStore) {
+    this.storeBank[name] = nextStore;
+  }
+
+  update(name, nextStore) {
+    let store = this.get(name);
+
+    if (!store) {
+      this.set(name, {});
+      store = this.get(name);
+    }
+
+    const oldStore = _.clone(store);
+
+    assign(store, nextStore);
+
+    this.emit(`update_${name}`, {
+      newStore: store,
+      oldStore: oldStore
+    });
   }
 }
 
 export const stores = new Stores();
 
-export function useStore(to, initial = {}) {
-  /* Handle initial state */
-  if (isUndefined(stores.get(to))) stores.update(to, initial);
+/**
+ * Creates a store with ethe given name and initials
+ *
+ * Example:
+ * createStore('store-name', {foo: 'bar'})
+ *
+ * @param {String} to Store name
+ * @param {Object} initial Initial data
+ */
+export function createStore(to, initial = {}) {
+  if (!isUndefined(stores.get(to)))
+    throw new Error(`Store ${to} was already created`);
 
-  /* This state only stores a random value just to trigger re-rendering on-demand.
-    State always comes from stores */
+  stores.update(to, initial);
+}
+
+/**
+ * Hook for binding a store to a component
+ *
+ * Example:
+ * const [store, setStore] = useStore('store-name')
+ *
+ * @param {*} to Store name
+ * @param {Function} condition Effect condition control (oldStore, newStore) => Boolean
+ */
+export function useStore(to, condition) {
+  const store = stores.get(to);
+
+  if (isUndefined(store))
+    throw new Error(`Store ${to} is not defined`);
+
+  /* This store only holds a random value just to trigger re-rendering on-demand.
+    Store always comes from stores */
   const [shit, setState] = useState(Date.now()); //eslint-disable-line
-  const set = nextState => stores.update(to, nextState);
+  const set = nextStore => stores.update(to, nextStore);
 
   useEffect(() => {
-    const updateListener = () => setState(Date.now()); // Trigger a re-render
+    const updateListener = ({oldStore, newStore}) => {
+      if (!isFunction(condition)) {
+        /* If no condition isprovided immediately trigger a re-render */
+        setState(Date.now());
+      } else {
+        /* Trigger a re-render only if condition returns true */
+        if (condition(oldStore, newStore))
+          setState(Date.now());
+      }
+    };
 
     stores.on(`update_${to}`, updateListener);
     return () => stores.removeListener(`update_${to}`, updateListener);
   });
 
-  return [stores.get(to), set];
+  return [store, set];
 }
+
+/**
+ * A syntactic sugar for the condition function.
+ *
+ * Example:
+ * useStore('store-name', whenChanged(['property1', 'property2']))
+ *
+ * @param {string[]} fields
+ */
+export function whenChanged(fields = []) {
+  return (oldStore, newStore) => {
+    return fields.some(field => {
+      const compiledField = _.template(field)(newStore);
+
+      return !isEquivalent(get(newStore, compiledField), get(oldStore, compiledField));
+    });
+  };
+};
---
+++ src/utils.js
@@ -0,0 +1,32 @@
+import isObject from 'lodash/isObject';
+import isEqual from 'lodash/isEqual';
+
+export const isEquivalent = (a = {}, b = {}) => {
+  if (!isObject(a) && !isObject(b)) {
+    return isEqual(a, b);
+  }
+
+  // Create arrays of property names
+  const aProps = Object.getOwnPropertyNames(a);
+  const bProps = Object.getOwnPropertyNames(b);
+
+  // If number of properties is different,
+  // objects are not equivalent
+  if (aProps.length !== bProps.length) {
+    return false;
+  }
+
+  for (var i = 0; i < aProps.length; i++) {
+    var propName = aProps[i];
+
+    // If values of same property are not equal,
+    // objects are not equivalent
+    if (a[propName] !== b[propName]) {
+      return false;
+    }
+  }
+
+  // If we made it this far, objects
+  // are considered equivalent
+  return true;
+};
